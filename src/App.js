cc98aad28f173968dabc8c09ddc59d8a4194d14c
Mark Dino <markdinopelonia447@gmail.com>
2019-10-17 09:59:11 +0800

    Add Layout component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./sass/main.scss";
-import Header from "./components/header";
 import Hero from "./components/hero";
 import Overview from "./components/overview";
 import Tutorial from "./components/tutorial";
 import Features from "./components/features";
 import LatestBlog from "./components/latestBlog";
 import About from "./components/about";
-import Footer from "./components/footer";
+import Layout from "./components/layout";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Hero />
-      <Overview />
-      <Tutorial />
-      <Features />
-      <LatestBlog />
-      <About />
-      <Footer />
+      <Layout>
+        <Hero />
+        <Overview />
+        <Tutorial />
+        <Features />
+        <LatestBlog />
+        <About />
+      </Layout>
     </div>
   );
 }
