8879d6463bbbaddf314daf670492f840878904d1
Mark Dino <markdinopelonia447@gmail.com>
2019-10-21 17:58:03 +0800

    icon and  apple-touch-icon are no longer needed

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,10 +1,6 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 
-import logo512 from "../images/meta/logo512.png"
-import logo192 from "../images/meta/logo192.png"
-import favicon from "../images/meta/favicon.ico"
-
 const Seo = ({ title }) => {
   return (
     <>
@@ -12,9 +8,6 @@ const Seo = ({ title }) => {
         <title>{`Gala App${title ? ` | ${title}` : ``}`}</title>
         <meta name="robots" content="index, follow" />
         <meta name="author" content="Project Semicolon" />
-        <link rel="apple-touch-icon" sizes="512x512" href={logo512} />
-        <link rel="apple-touch-icon" sizes="192x192" href={logo192} />
-        <link rel="icon" href={favicon} />
       </Helmet>
     </>
   )
