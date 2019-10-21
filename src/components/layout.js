6177a3df6fe4e2e6e806e1efa83883bfabba8ecb
Mark Dino <markdinopelonia447@gmail.com>
2019-10-21 21:51:15 +0800

    move Seo component from Layout to index

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,15 @@
-import React from "react";
-import Header from "./header";
-import Footer from "./footer";
-import Seo from "./seo";
+import React from "react"
+import Header from "./header"
+import Footer from "./footer"
 
-const Layout = ({ children, title }) => {
+const Layout = ({ children }) => {
   return (
     <>
-      <Seo title={title} />
       <Header />
       {children}
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
