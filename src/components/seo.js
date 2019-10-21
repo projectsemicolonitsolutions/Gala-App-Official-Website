bf942bede41f020e9eb231e1050719e158bfdd4d
Mark Dino <markdinopelonia447@gmail.com>
2019-10-21 21:50:03 +0800

    uses static query to get data of siteMetadata

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,13 +1,32 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
 
-const Seo = ({ title }) => {
+const Seo = ({ pageTitle, description }) => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
+        }
+      }
+    `
+  )
+
+  const metaDescription = description || site.siteMetadata.description
+  const { title, author } = site.siteMetadata
+
   return (
     <>
       <Helmet>
-        <title>{`Gala App${title ? ` | ${title}` : ``}`}</title>
+        <title>{`${pageTitle ? `${pageTitle} | ` : ``}${title}`}</title>
+        <meta name="description" content={metaDescription} />
         <meta name="robots" content="index, follow" />
-        <meta name="author" content="Project Semicolon" />
+        <meta name="author" content={author} />
       </Helmet>
     </>
   )
