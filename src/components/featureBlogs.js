0f4584a0599d4fac1a3efe46d163d2cc360f147f
Mark Dino <markdinopelonia447@gmail.com>
2019-10-30 08:55:16 +0800

    feat: FetureBlogs section

---
+++ src/components/featureBlogs.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Row, Col } from "reactstrap"
+import FeatureCard from "./shared-components/featureCard"
+
+const FeatureBlogs = ({ blogs }) => {
+  return (
+    <section className="feature-blogs">
+      <Row className="m-0 mt-5 mb-3 pt-5 px-3 flex-nowrap overflow-auto">
+        {blogs.map(blog => (
+          <Col md="4" sm="6" xs="12" className="">
+            <FeatureCard featBlog={blog} />
+          </Col>
+        ))}
+      </Row>
+    </section>
+  )
+}
+
+export default FeatureBlogs
