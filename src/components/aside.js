0e46b3eabecc5fbff69e6caaefa27beba5d20898
Mark Dino <markdinopelonia447@gmail.com>
2019-11-06 14:32:34 +0800

    add className thru props to add more classNames

--- src/components/aside.js
+++ src/components/aside.js
@@ -1,7 +1,11 @@
 import React from "react"
 
-const Aside = ({ children }) => {
-  return <aside className="w-100 mb-5 px-lg-4">{children}</aside>
+const Aside = ({ children, className }) => {
+  return (
+    <aside className={`w-100 mb-5 px-lg-4 ${className || ``}`}>
+      {children}
+    </aside>
+  )
 }
 
 export default Aside
