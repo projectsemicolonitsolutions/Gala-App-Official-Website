a971b6734a2cab1680fea04b2c65132cb86e519b
Mark Dino <markdinopelonia447@gmail.com>
2019-11-06 18:09:04 +0800

    feat: RecentBlogs comonent for the list of recent blog post

---
+++ src/components/recentBlogs.js
@@ -0,0 +1,17 @@
+import React from "react"
+import BlogAsideSM from "./shared-components/blogAsideSM"
+
+const RecentBlogs = ({ blogs }) => {
+  const recentblog = blogs.slice(4, 8)
+  return (
+    <ul className="list-unstyled">
+      {recentblog.map(blog => (
+        <li>
+          <BlogAsideSM blog={blog} />
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default RecentBlogs
