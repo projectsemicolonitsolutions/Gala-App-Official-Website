1b98187ba3913b05c8d09b41cd883c97c6f2aef5
Mark Dino <markdinopelonia447@gmail.com>
2019-11-07 09:35:00 +0800

    core: use gatsby-plugin-layout to use Layout component in every pages without importing it

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
 import { Row, Col, Container } from "reactstrap"
-import Layout from "../components/layout"
 import Seo from "../components/seo"
 import FeatureBlogs from "../components/featureBlogs"
 import AllBlogs from "../components/allBlogs"
@@ -17,49 +16,47 @@ const BlogPage = ({ data }) => {
   const allBlogs = data.all.nodes
   return (
     <>
-      <Layout>
-        <Seo pageTitle="Blog" />
-        <FeatureBlogs blogs={featuredBlogs} />
-        <Container>
-          <Row className="py-5 flex-lg-row flex-column-reverse">
-            <Col lg="8" xs="12">
-              <AllBlogs blogs={allBlogs} />
-            </Col>
-            <Col lg="4" xs="12">
-              <Aside>
-                <Row className="flex-lg-column flex-md-row-reverse flex-column-reverse">
-                  <Col
-                    lg="12"
-                    md={{ size: 4, offset: 0 }}
-                    xs={{ size: 7, offset: 5 }}
-                    className="d-flex justify-content-end"
-                  >
-                    <BlogSearch />
-                  </Col>
-                  <Col lg="12" md="8" xs="12">
-                    <HeaderAside title="category" />
-                    <Categories blogs={allBlogs} />
-                  </Col>
-                </Row>
-              </Aside>
-              <Aside className="d-lg-flex d-none">
-                <Row>
-                  <Col xs="12">
-                    <HeaderAside title="recent post" />
-                    <RecentBlogs blogs={allBlogs} />
-                  </Col>
-                </Row>
-              </Aside>
-            </Col>
-          </Row>
-          <Row className="d-lg-none">
-            <Col xs={{ size: 10, offset: 1 }} md={{ size: 8, offset: 2 }}>
-              <HeaderAside title="recent post" />
-              <RecentBlogs blogs={allBlogs} />
-            </Col>
-          </Row>
-        </Container>
-      </Layout>
+      <Seo pageTitle="Blog" />
+      <FeatureBlogs blogs={featuredBlogs} />
+      <Container>
+        <Row className="py-5 flex-lg-row flex-column-reverse">
+          <Col lg="8" xs="12">
+            <AllBlogs blogs={allBlogs} />
+          </Col>
+          <Col lg="4" xs="12">
+            <Aside>
+              <Row className="flex-lg-column flex-md-row-reverse flex-column-reverse">
+                <Col
+                  lg="12"
+                  md={{ size: 4, offset: 0 }}
+                  xs={{ size: 7, offset: 5 }}
+                  className="d-flex justify-content-end"
+                >
+                  <BlogSearch />
+                </Col>
+                <Col lg="12" md="8" xs="12">
+                  <HeaderAside title="category" />
+                  <Categories blogs={allBlogs} />
+                </Col>
+              </Row>
+            </Aside>
+            <Aside className="d-lg-flex d-none">
+              <Row>
+                <Col xs="12">
+                  <HeaderAside title="recent post" />
+                  <RecentBlogs blogs={allBlogs} />
+                </Col>
+              </Row>
+            </Aside>
+          </Col>
+        </Row>
+        <Row className="d-lg-none">
+          <Col xs={{ size: 10, offset: 1 }} md={{ size: 8, offset: 2 }}>
+            <HeaderAside title="recent post" />
+            <RecentBlogs blogs={allBlogs} />
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,22 +7,19 @@ import Tutorial from "../components/tutorial"
 import Features from "../components/features"
 import LatestBlog from "../components/latestBlog"
 import About from "../components/about"
-import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const IndexPage = ({ data }) => {
   const latestBlog = data.latest.nodes
   return (
     <>
-      <Layout>
-        <Seo pageTitle="Home" />
-        <Hero />
-        <Overview />
-        <Tutorial />
-        <Features />
-        <LatestBlog blogs={latestBlog} />
-        <About />
-      </Layout>
+      <Seo pageTitle="Home" />
+      <Hero />
+      <Overview />
+      <Tutorial />
+      <Features />
+      <LatestBlog blogs={latestBlog} />
+      <About />
     </>
   )
 }
